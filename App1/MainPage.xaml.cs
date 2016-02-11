030ca8e5e9daf4a589895fe3558166719c9a0bfc
Lev Ginsburg <lev.ginsburg@garmin.com>
2016-02-11 13:18:12 -0600

    initial code commit

---
+++ App1/MainPage.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Notifications;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using Microsoft.QueryStringDotNET;
+using NotificationsExtensions.Toasts;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Sample
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainViewModel Vm => DataContext as MainViewModel;
+
+        public MainPage()
+        {
+            this.DataContext = new MainViewModel();
+            this.InitializeComponent();
+        }
+    }
+
+    public class MainViewModel : ViewModelBase
+    {
+        private readonly Random m_random = new Random();
+        private int m_value;
+
+        public int Value
+        {
+            get { return m_value; }
+            set { Set(nameof(Value), ref m_value, value); }
+        }
+
+        public ICommand NewNumberCommand { get; }
+
+        public ICommand CreateToastCommand { get; }
+
+        public MainViewModel()
+        {
+            NewNumberCommand = new RelayCommand(() =>
+            {
+                Value = m_random.Next(0, 100);
+            });
+            CreateToastCommand = new RelayCommand(CreateToast);
+        }
+
+        public void CreateToast()
+        {
+            ToastVisual visual = new ToastVisual
+            {
+                TitleText = new ToastText {Text = "Sample notification"},
+                BodyTextLine1 = new ToastText {Text = $"Your latest random number: {Value}!"}
+            };
+            ToastActionsCustom actions = new ToastActionsCustom
+            {
+                Buttons =
+                {
+                    new ToastButton("Delete", new QueryString
+                    {
+                        {"action", "delete"},
+                        {"id", Value.ToString()},
+                    }.ToString()),
+                    new ToastButton("Get a new one", new QueryString
+                    {
+                        {"action", "regenerate"},
+                        {"id", Value.ToString()}
+                    }.ToString())
+                },
+            };
+            ToastContent content = new ToastContent
+            {
+                Visual = visual,
+                Actions = actions,
+            };
+            ToastNotification toast = new ToastNotification(content.GetXml())
+            {
+                ExpirationTime = DateTime.Now.AddDays(2),
+                Tag = Value.ToString(),
+                Group = "Random Numbers!"
+            };
+
+            ToastNotificationManager.CreateToastNotifier().Show(toast);
+        }
+    }
+}
---
+++ VoiceCommandService/SampleVoiceCommandService.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.AppService;
+using Windows.ApplicationModel.Background;
+using Windows.ApplicationModel.VoiceCommands;
+using Windows.Media.SpeechRecognition;
+
+namespace Sample.VoiceCommands
+{
+    public sealed class SampleVoiceCommandService : IBackgroundTask
+    {
+        private readonly Random m_random = new Random();
+
+        private BackgroundTaskDeferral m_serviceDeferral;
+        private VoiceCommandServiceConnection m_voiceServiceConnection;
+
+        public async void Run(IBackgroundTaskInstance taskInstance)
+        {
+            m_serviceDeferral = taskInstance.GetDeferral();
+
+            // Register to receive an event if Cortana dismisses the background task. This will
+            // occur if the task takes too long to respond, or if Cortana's UI is dismissed.
+            // Any pending operations should be cancelled or waited on to clean up where possible.
+            taskInstance.Canceled += OnTaskCanceled;
+
+            var triggerDetails = taskInstance.TriggerDetails as AppServiceTriggerDetails;
+            if (triggerDetails?.Name == "SampleVcs")
+            {
+                try
+                {
+                    m_voiceServiceConnection = VoiceCommandServiceConnection.FromAppServiceTriggerDetails(triggerDetails);
+                    m_voiceServiceConnection.VoiceCommandCompleted += OnVoiceCommandCompleted;
+
+                    VoiceCommand voiceCommand = await m_voiceServiceConnection.GetVoiceCommandAsync();
+                    var command = voiceCommand?.CommandName ?? "noCommand";
+
+                    switch (command)
+                    {
+                        case "randomNumber":
+                            GetRandomNumber();
+                            break;
+
+                        default:
+                            LaunchApp();
+                            break;
+                    }
+
+                    m_serviceDeferral.Complete();
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+        }
+
+        private async void GetRandomNumber()
+        {
+            var number = m_random.Next(100);
+
+            var userMessage = new VoiceCommandUserMessage();
+            userMessage.DisplayMessage = $"Random number: {number}";
+            userMessage.SpokenMessage = $"I picked {number}";
+
+            var response = VoiceCommandResponse.CreateResponse(userMessage);
+            await m_voiceServiceConnection.ReportSuccessAsync(response);
+        }
+
+        private async void LaunchApp()
+        {
+            var userMessage = new VoiceCommandUserMessage();
+            userMessage.SpokenMessage = "Launching Sample app";
+
+            var response = VoiceCommandResponse.CreateResponse(userMessage);
+            await m_voiceServiceConnection.RequestAppLaunchAsync(response);
+        }
+
+        private void OnVoiceCommandCompleted(VoiceCommandServiceConnection sender, VoiceCommandCompletedEventArgs args)
+        {
+            m_serviceDeferral?.Complete();
+        }
+
+
+        private void OnTaskCanceled(IBackgroundTaskInstance sender, BackgroundTaskCancellationReason reason)
+        {
+            m_serviceDeferral?.Complete();
+        }
+    }
+}
